cb94734bc86b51008fcf6d7388f72a4483c431d1
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-04-27 06:48:02 -0400

    create files

---
+++ src/main/java/com/coderscampus/nestorassignment14/NestorAssignment14Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.nestorassignment14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestorAssignment14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestorAssignment14Application.class, args);
+	}
+
+}
