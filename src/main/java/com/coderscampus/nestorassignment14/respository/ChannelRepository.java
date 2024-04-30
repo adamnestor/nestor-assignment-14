8fbfd261f7ee9df1c03465f46fb15c38eb061cf5
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-04-30 16:42:50 -0400

    Update ChannelRepository.java

--- src/main/java/com/coderscampus/nestorassignment14/respository/ChannelRepository.java
+++ src/main/java/com/coderscampus/nestorassignment14/respository/ChannelRepository.java
@@ -13,11 +13,33 @@ public class ChannelRepository {
 	List<Channel> channels = new ArrayList<>();
 
 	public ChannelRepository() {
-		Channel general = new Channel();
-		general.setChannelId(1L);
-		general.setName("General");
+		// Adding General Channel
+		Channel generalChannel = new Channel();
+		generalChannel.setChannelId(1L);
+		generalChannel.setName("General");
 
-		channels.add(general);
+		channels.add(generalChannel);
+
+		//// Decided to add additional channels to test functionality of rending
+		//// channels only to channels they were sent on. Now feeling the pressure to
+		//// make a method to create new channels instead of hardcoding. Maybe a future
+		//// refactoring.
+
+		// Adding Coding Channel
+
+		Channel codingChannel = new Channel();
+		codingChannel.setChannelId(2L);
+		codingChannel.setName("Coding");
+
+		channels.add(codingChannel);
+
+		// Adding Coding Channel
+		Channel sportsChannel = new Channel();
+		sportsChannel.setChannelId(3L);
+		sportsChannel.setName("Sports");
+
+		channels.add(sportsChannel);
+
 	}
 
 	public Channel findById(Long channelId) {
